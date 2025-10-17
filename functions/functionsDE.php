711de2132b8d2578419b1084814878b848883381
diurno <diurno@B27-B3.tierno.es>
2025-10-17 14:28:55 +0200

    functios 1.0

--- functions/functionsDE.php
+++ functions/functionsDE.php
@@ -1,6 +1,45 @@
 <?php
 
 function filterByType( array $arr, $tipo): array{
-    
+    switch($tipo){
+
+            //array.splice(indice, 1)
+        case "par":
+            for($i = 0; $i <= count($arr); $i++){
+                if(($arr[$i] % 2) != 0){
+                    //unset elimina
+                    unset($arr[$i]);
+                }
+            }
+        break;
+
+        case "impar":
+            for($i = 0; $i <= count($arr); $i++){
+                if(($arr[$i] % 2) == 0){
+                    unset($arr[$i]);
+                }
+            }
+        break;
+
+        case "primo":
+            
+        break;
+
+        case "positivo":
+            for($i = 0; $i <= count($arr); $i++){
+                if($arr[$i] < 0){
+                    unset($arr[$i]);
+                }
+            }
+        break;
+
+        case "negativo":
+            for($i = 0; $i <= count($arr); $i++){
+                if($arr[$i] >= 0){
+                    unset($arr[$i]);
+                }
+            }
+        break;
+    }
     return $arr;
 }
