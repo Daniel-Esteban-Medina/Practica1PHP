633a808f1284de5d7d09a1804f2c0ef2dff5be95
diurno <diurno@B27-B3.tierno.es>
2025-10-20 13:30:53 +0200

    Lunes 13:30

--- functions/functionsDE.php
+++ functions/functionsDE.php
@@ -72,22 +72,55 @@ function calculateStatistics( array $arr): array{
     sort($arr);
     if((count($arr) % 2) == 0){
         $num1 = count($arr)/2;
-        $num2 = (count($arr)/2)+1;
+        $num2 = (count($arr)/2)-1;
         $mediana = ($arr[$num1]+$arr[$num2])/2;
 
     } else {
-        $medianaIndice = (count($arr)+1)/2;
+        $medianaIndice = (count($arr)-1)/2;
         $mediana = $arr[$medianaIndice];
     }
     $arrayAso["Mediana"] = $mediana;
 
+    $repeticiones = array_count_values($arr);
+    $mayor = max($repeticiones);
+    $modaArray = array_keys($repeticiones, $mayor);
+    $arrayAso["Moda"] = $modaArray[0];
 
     return $arrayAso;
 }
 
 // 3 
 function analizarPalabras($texto):array{
-    $arr = [];
-    $numLetras = 0;
+    $arr = [
+        "numPalabras" => 0,
+        "larga" => "",
+        "corta" => ""
+    ];
+
+    $palabras = explode(" ", $texto);
+    $arr["numPalabras"] = count($palabras);
+    $larga = "";
+    $corta = "";
+    $grande = 0; $peque = count(str_split($texto));
+    for($i = 0; $i < count($palabras); $i++){
+        if(strlen($palabras[$i]) < $peque){
+            $peque = strlen($palabras[$i]);
+            $corta = $palabras[$i];
+        }
+
+        if(strlen($palabras[$i]) > $grande){
+            $grande = strlen($palabras[$i]);
+            $larga = $palabras[$i];
+        }
+    }
+    $arr["larga"] = $larga;
+    $arr["corta"] = $corta;
+
     return $arr;
+}
+
+// 4
+
+function convertTemperature(){
+    
 }
